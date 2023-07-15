8126dd2dce4999c47132249bfa9ffde575ba6de4
Anas Badr <anasihabezmc@gmail.com>
2023-07-15 22:22:57 +0300

    made a constants module and added all of the constants to it

---
+++ src/constants.rs
@@ -0,0 +1,23 @@
+use eframe::egui::Color32;
+use eframe::egui::vec2;
+
+pub const WIN_WIDTH: f32 = 390.0;
+pub const WIN_HEIGHT: f32 = 550.0;
+
+pub const HEADING_FONT_SIZE: f32 = 50.0;
+pub const NORMAL_FONT_SIZE: f32 = 15.0;
+pub const PLACEHOLDER_FONT_SIZE: f32 = 25.0;
+pub const CHECKBOX_TEXT_FONT_SIZE: f32 = 18.0;
+
+pub const TRANSPARENT: Color32 = Color32::from_rgba_premultiplied(0, 0, 0, 0);
+pub const DARK_GREY: Color32 = Color32::from_rgb(96, 96, 96);
+
+pub const TEXTBOX_WIDTH: f32 = 350.0;
+
+pub const VERTICAL_SPACING: f32 = 20.0;
+pub const HORIZONTAL_SPACING: f32 = 20.0;
+
+pub const SPACING_BETWEEN_BUTTONS: f32 = 12.0;
+pub const BUTTON_ROUNDING: f32 = 4.0;
+
+pub const IMAGE_DIMENSIONS: egui::Vec2 = vec2(15.0, 15.0);
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,13 @@
+mod constants;
+
+use crate::constants::*;
+
 use eframe::egui::RichText;
 use eframe::egui::{vec2, Color32, Context, FontId};
 use eframe::{egui, run_native, Frame};
 use egui_extras::RetainedImage;
 
-const WIN_WIDTH: f32 = 390.0;
-const WIN_HEIGHT: f32 = 550.0;
 
-const HEADING_FONT_SIZE: f32 = 50.0;
-const NORMAL_FONT_SIZE: f32 = 15.0;
-const PLACEHOLDER_FONT_SIZE: f32 = 25.0;
-const CHECKBOX_TEXT_FONT_SIZE: f32 = 18.0;
-
-const TRANSPARENT: Color32 = Color32::from_rgba_premultiplied(0, 0, 0, 0);
-const DARK_GREY: Color32 = Color32::from_rgb(96, 96, 96);
-
-const TEXTBOX_WIDTH: f32 = 350.0;
-
-const VERTICAL_SPACING: f32 = 20.0;
-const HORIZONTAL_SPACING: f32 = 20.0;
-
-const SPACING_BETWEEN_BUTTONS: f32 = 12.0;
-const BUTTON_ROUNDING: f32 = 4.0;
-
-const IMAGE_DIMENSIONS: egui::Vec2 = vec2(15.0, 15.0);
 
 fn main() {
     let native_options = eframe::NativeOptions {
